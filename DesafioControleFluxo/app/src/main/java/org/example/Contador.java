1128d1a3bfeb8ef790af674c1c873e6c7d5e8c77
Thales Nishida <nishida.tn@proton.me>
2025-06-13 08:21:52 -0300

    first commit

---
+++ DesafioControleFluxo/app/src/main/java/org/example/Contador.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Scanner;
+import org.example.exceptions.ParametrosInvalidosException;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("Parâmetros inválidos");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
---
+++ DesafioControleFluxo/app/src/main/java/org/example/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package org.example.exceptions;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
